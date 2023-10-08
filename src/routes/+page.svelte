0b9d85579201b2972d7ca34ed7ccd530fa44f72f
Yogesh Thambidurai <gyogesh2015@outlook.com>
2023-10-08 12:11:09 -0400

    feat: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<main>
+  <h1>Hello World</h1>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    font-family: sans-serif;
+  }
+</style>
